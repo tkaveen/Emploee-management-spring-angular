8881ced55c4a861ac9618b553f5207561e0478b1
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-09-08 15:25:32 +0530

    GET,POST,PUT,DELETE Methods done

---
+++ emploeemanagement/src/main/java/com/empmgt/emploeemanagement/EmployeeResource.java
@@ -0,0 +1,55 @@
+package com.empmgt.emploeemanagement;
+
+import com.empmgt.emploeemanagement.model.Employee;
+import com.empmgt.emploeemanagement.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    //  Get All Employees
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    //  Find Employee By Id
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    //  Add Employee
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    //  update Employee
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updtEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updtEmployee, HttpStatus.OK);
+    }
+
+    //  Delete Employee
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
