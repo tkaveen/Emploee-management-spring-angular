6ab51eecdc5cda53852d3844f101a55a8334c68c
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-09-09 13:25:51 +0530

    Tab icon added

---
+++ emploeemanagement/src/main/java/com/empmgt/emploeemanagement/EmploeemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.empmgt.emploeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploeemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploeemanagementApplication.class, args);
+	}
+
+}
---
+++ emploeemanagement/src/test/java/com/empmgt/emploeemanagement/EmploeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empmgt.emploeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
