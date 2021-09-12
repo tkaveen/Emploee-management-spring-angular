9c1937878725841f343cdf0bd65aa2716aa0280e
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-09-12 19:16:43 +0530

    Add,update,delete modal added

--- employeemanagement-fontend-app/src/app/app.component.ts
+++ employeemanagement-fontend-app/src/app/app.component.ts
@@ -27,4 +27,23 @@ export class AppComponent implements OnInit {
       }
     );
   }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
